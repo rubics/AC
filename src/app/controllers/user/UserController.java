c9e67cd3835f2f074c132ac2bb15fafd49168046
Pratuat <pratuat@gmail.com>
2012-07-07 10:55:54 +0545

    working on UserController.httpSuccess()

--- src/app/controllers/user/UserController.java
+++ src/app/controllers/user/UserController.java
@@ -1,6 +1,7 @@
 package app.controllers.user;
 
 import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.Dialog;
 import app.AirCrewApp;
 import app.models.SigninRequest;
 import app.models.User;
@@ -16,15 +17,38 @@ public class UserController {
 	public SigninRequest signinRequest = new SigninRequest() {
 
 		public void httpsuccess(byte[] array, String str){
+
 			String json_response = new String(array);
+			System.out.println(json_response);
+			int temp = json_response.indexOf("a");
 			
-//			AirCrewApp.app.invokeLater(new Runnable() {
+			if (UiApplication.getUiApplication() != null){
+				System.out.println(">> NOT NULL");
+				System.out.println();
+			}else {
+				System.out.println(">> NULL");
+				System.out.println();
+			}
+			
+			UiApplication.getUiApplication().invokeAndWait(new Runnable() {
+				
+				public void run() {
+					Dialog.alert("Hello from AirCrew :-)");				
+				}
+			});
+			
+			
+//			try{
+//			UiApplication.getUiApplication().invokeAndWait(new Runnable() {
 //				
 //				public void run() {
 //					UiApplication.getUiApplication().pushScreen(new DashboardScreen());
-////					UiApplication.getUiApplication().popScreen(signinScreen);
+//					UiApplication.getUiApplication().popScreen(signinScreen);
 //				}
-//			});
+//			});} catch(Exception ex){
+//				System.out.println("Exception @ UserController.httpsuccess : " + ex.getClass().getName());
+//				ex.printStackTrace();
+//			}
 			
 //			try {
 //				JSONObject json = new JSONObject(json_response);
